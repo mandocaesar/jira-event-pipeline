6ed949ffde02743485aea7bceb33d78af0aafb1d
armand caesar <armand.caesar@gmail.com>
2020-01-27 02:40:35 +0700

    fix rdd

--- spark-consumer.py
+++ spark-consumer.py
@@ -21,12 +21,12 @@ if __name__ == "__main__":
 
     def process(time, rdd):
       if rdd.isEmpty() == False:
-        
-        spark.read.format('org.apache.kudu.spark.kudu').option('kudu.master',kuduMasters)\
+        rdd.collect().foreach(val => 
+          spark.read.format('org.apache.kudu.spark.kudu').option('kudu.master',kuduMasters)\
              .option('kudu.table',kuduTableName).load().registerTempTable(kuduTableName)
-        #insert into default.jira_events values (uuid(), localtimestamp, '')
-        spark.sql("INSERT INTO TABLE `" + kuduTableName + "` (uuid(),  localtimestamp, `" + rdd.values()[0]+"`)")
-
+          #insert into default.jira_events values (uuid(), localtimestamp, '')
+          spark.sql("INSERT INTO TABLE `" + kuduTableName + "` (uuid(),  localtimestamp, `" + val +"`)")
+        )
         # PySpark KuduContext not yet available (https://issues.apache.org/jira/browse/KUDU-1603)
 
     windowedStream.foreachRDD(process)
