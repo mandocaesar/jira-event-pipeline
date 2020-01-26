9d8c80c249555856fd210c97752f7697b1463681
armand caesar <armand.caesar@gmail.com>
2020-01-27 03:08:06 +0700

    debug

--- spark-consumer.py
+++ spark-consumer.py
@@ -24,13 +24,12 @@ if __name__ == "__main__":
     def process(time, rdd):
         if rdd.isEmpty() == False:
             collection = rdd.collect()
-            print(collection[0][1])
             spark.read.format('org.apache.kudu.spark.kudu').option('kudu.master', kuduMasters)\
                  .option('kudu.table', kuduTableName).load().registerTempTable(kuduTableName)
             # insert into default.jira_events values (uuid(), localtimestamp, '')
-            str = ''.join(collection)
+            str = ''.join(collection[0][1])
             spark.sql("INSERT INTO TABLE `" + kuduTableName +
-                      "` (uuid(),  localtimestamp, `" + collection[0] + collection[0][1] + "`)")
+                      "` (uuid(),  localtimestamp, `" + collection[0] + str + "`)")
 
             # PySpark KuduContext not yet available (https://issues.apache.org/jira/browse/KUDU-1603)
 
