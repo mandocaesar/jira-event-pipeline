80454e736d90cdaf9aedf332db98312a00245788
armand caesar <armand.caesar@gmail.com>
2020-01-27 03:27:09 +0700

    fix sql syntax

--- spark-consumer.py
+++ spark-consumer.py
@@ -34,7 +34,8 @@ if __name__ == "__main__":
                  .option('kudu.table', kuduTableName).load().registerTempTable(kuduTableName)
             # insert into default.jira_events values (uuid(), localtimestamp, '')
             str = ''.join(collection[0][1])
-            spark.sql("INSERT INTO TABLE {table} values (uuid(), {timestamp}), {payload}".format(table=kuduTableName, timestamp))
+            spark.sql("INSERT INTO TABLE {table} values (uuid(), {timestamp}), {payload}".format(
+                table=kuduTableName, timestamp=timestamp))
             # spark.sql("INSERT INTO TABLE `" + kuduTableName +
             #           "` values (uuid(), `" + unix_timestamp() +`",`" + str + "`)")
 
