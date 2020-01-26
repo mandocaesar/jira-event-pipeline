f0201647f3dffdaa9d95acf7b69facc3ce988fcd
armand caesar <armand.caesar@gmail.com>
2020-01-27 02:46:45 +0700

    fix rdd with foreach

--- spark-consumer.py
+++ spark-consumer.py
@@ -11,23 +11,26 @@ if __name__ == "__main__":
 
     kuduTableName = "jira_events"
     kafkaBrokers, kuduMasters = sys.argv[1:]
-    topicSet =  ["jira-event"]
+    topicSet = ["jira-event"]
 
     spark = SparkSession.builder.appName("KafkaToKuduPython").getOrCreate()
     ssc = StreamingContext(spark.sparkContext, 5)
 
-    dstream = KafkaUtils.createDirectStream(ssc, topicSet, {"metadata.broker.list": kafkaBrokers})
+    dstream = KafkaUtils.createDirectStream(
+        ssc, topicSet, {"metadata.broker.list": kafkaBrokers})
     windowedStream = dstream.window(60)
 
     def process(time, rdd):
-      if rdd.isEmpty() == False:
-        rdd.collect().foreach(val => 
-          spark.read.format('org.apache.kudu.spark.kudu').option('kudu.master',kuduMasters)\
-             .option('kudu.table',kuduTableName).load().registerTempTable(kuduTableName)
-          #insert into default.jira_events values (uuid(), localtimestamp, '')
-          spark.sql("INSERT INTO TABLE `" + kuduTableName + "` (uuid(),  localtimestamp, `" + val +"`)")
-        )
-        # PySpark KuduContext not yet available (https://issues.apache.org/jira/browse/KUDU-1603)
+        if rdd.isEmpty() == False:
+            rdd.collect().foreach(val=>  {
+                spark.read.format('org.apache.kudu.spark.kudu').option(
+                    'kudu.master', kuduMasters)
+                .option('kudu.table', kuduTableName).load().registerTempTable(kuduTableName)
+                # insert into default.jira_events values (uuid(), localtimestamp, '')
+                spark.sql("INSERT INTO TABLE `" + kuduTableName + \
+                          "` (uuid(),  localtimestamp, `" + val + "`)")
+            })
+            # PySpark KuduContext not yet available (https://issues.apache.org/jira/browse/KUDU-1603)
 
     windowedStream.foreachRDD(process)
 
