c0c93bf110fb748f4dc4efd68221eb7f5c2260fc
armand caesar <armand.caesar@gmail.com>
2020-01-27 03:20:40 +0700

    fix sql syntax

--- spark-consumer.py
+++ spark-consumer.py
@@ -6,7 +6,6 @@ from pyspark.sql import SparkSession
 from collections import defaultdict
 from datetime import datetime
 
-print("timestamp =", timestamp)
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: kafka_to_kudu.py <kafka-brokers> <kudu-masters>")
@@ -35,7 +34,7 @@ if __name__ == "__main__":
             # insert into default.jira_events values (uuid(), localtimestamp, '')
             str = ''.join(collection[0][1])
             spark.sql("INSERT INTO TABLE `" + kuduTableName +
-                      "` values (uuid(), `" + timestamp + "`+  unix_timestamp(), `" + str + "`)")
+                      "` values (uuid(), `" + timestamp + "`+  `", `" + str + "`)")
 
             # PySpark KuduContext not yet available (https://issues.apache.org/jira/browse/KUDU-1603)
 
